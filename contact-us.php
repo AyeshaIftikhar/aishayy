c912c59a75bc51687e6675b51abc3fd64bb18252
Ayesha Iftikhar <seAyeshaiftikhar@gmail.com>
2024-09-29 14:05:49 +0500

    topbar

--- contact-us.php
+++ contact-us.php
@@ -8,40 +8,29 @@
 <head>
 
     <!-- Basic Page Needs
-  ================================================== -->
+    ================================================== -->
     <title>CONTACT US</title>
     <meta charset="utf-8">
-    <meta name="author" content="">
+    <meta name="author" content="Ayesha Iftikhar">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="description" content="CONTACT US">
-    <meta name="keywords" content="CONTACT US">
-
-
+    <meta name="description"
+        content="Reach out to Aishayy today to discuss your mobile app development needs. We are located in the vibrant city of Lahore and eager to collaborate on your next big project.">
+    <meta name="keywords"
+        content="contact us, aishayy, mobile app development, flutter app development, android apps development, ios app development, mobile app design and development, cross-platform app development, cross platform apps, ai-powered mobile apps, app development for startups, mobile app performance optimization">
 
     <meta name="twitter:card" content="summary_large_image" />
 
-
-
-    <!-- Mobile Specific Metas
-  ================================================== -->
-
-
+    <!-- Mobile Specific Metas================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
     <script type="text/javascript" src="https://afarkas.github.io/lazysizes/lazysizes.min.js"></script>
     <!-- WEBSITE JS -->
-
-
     <!-- PAGE JS -->
     <!-- CSS ================================================== -->
-
     <link rel="stylesheet" href="css/combined.min.css?innove" type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="//static.sitemantic.com/webbuilder/js/lightbox/src/css/lightbox.css">
-
-
-
     <link id="theme" rel="stylesheet" href="css/default.css?innove" type='text/css'>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Exo+2:400,500,600,700,800,900%7CRaleway:400,500,600&amp;display=swap"
@@ -71,16 +60,11 @@
             js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0';
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
+    <!-- header -->
     <header class="blockHeader wb-header  " fixed="false">
         <div class="container boxWidget">
             <div class="logoWidget logo logoTextSettingClass     ">
                 <a href="default.php?innove" title="">
-
-
-
-
-
-
                     <span id="logoTextWrap" style="margin-top:8%;">
                         <span class="websiteLogoName"
                             style="color:#000;font-family:Open Sans;font-size:22px">aishayy</span>
@@ -88,7 +72,8 @@
                     </span>
                 </a>
             </div>
-            <div class="navbar navbar-default navbar-static-top  ">
+            <?php include 'topbar.php'; ?>
+            <!-- <div class="navbar navbar-default navbar-static-top  ">
                 <div class="navbar-header" id="729144" compid="729144" pageid="0" component-data="{}">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="sr-only">Toggle navigation</span>
@@ -97,71 +82,51 @@
                         <span class="icon-bar"></span>
                     </button>
                 </div>
-
-                <div class="navWidget navbar-collapse collapse
-        
-    ">
+                <div class="navWidget navbar-collapse collapse">
                     <ul class="parent-nav-ul nav navbar-nav cl-effect-1">
                         <li class="dropdown">
                             <a href="default.php" pageslug="home" linktype="Page" class="siteNavLink">
                                 HOME
                             </a>
                         </li>
-
-
                         <li class="dropdown">
                             <a href="about.php" pageslug="about" linktype="Page" class="siteNavLink">
                                 ABOUT
                             </a>
                         </li>
-
-
                         <li class="dropdown">
                             <a href="services.php" pageslug="services" linktype="Page" class="siteNavLink">
                                 SERVICES
                             </a>
                         </li>
-
-
                         <li class="dropdown">
                             <a href="gallery.php" pageslug="gallery" linktype="Page" class="siteNavLink">
                                 GALLERY
                             </a>
                         </li>
-
-
                         <li class="dropdown">
                             <a href="contact-us.php" pageslug="contact-us" linktype="Page" class="siteNavLink">
                                 CONTACT US
                             </a>
                         </li>
-
-
-
                     </ul>
                 </div>
-
-            </div>
-
-
+            </div> -->
         </div>
-
-
         <div class="container header-image-text"></div>
     </header>
+
+    <!--  -->
     <div id="contact-us" class="page wb-page">
-
-
 
         <section id="section-729126" class="
                 breadcrumb-section sec-padding-lg                 "
-            style="    background-image: url(https://images.pexels.com/photos/574069/pexels-photo-574069.jpeg?auto=compress&amp;cs=tinysrgb&amp;fit=crop&amp;h=627&amp;w=1200);    background-repeat: no-repeat;    background-size: cover;    background-position: center center;    background-attachment: fixed;">
+            style="background-image: url(https://images.pexels.com/photos/574069/pexels-photo-574069.jpeg?auto=compress&amp;cs=tinysrgb&amp;fit=crop&amp;h=627&amp;w=1200);    background-repeat: no-repeat;    background-size: cover;    background-position: center center;    background-attachment: fixed;">
             <div class="richTextWidget title-heading ">
                 <h1>Get In Touch</h1>
             </div>
-
         </section>
-        <section id="section-729128" class="
+        <!-- <section id="section-729128" class="
                 widget-section widget-information sec-padding-xl                 " style="">
             <div class="container boxWidget">
                 <div class="row ">
@@ -171,16 +136,10 @@
                                 marginwidth="0"
                                 src="https://maps.google.com/maps?q=123%20Mobile%20App%20Boulevard%2C%20New%20York%2C%20NY%2C%20USA&amp;z=16&amp;output=embed"></iframe>
                         </div>
-
                     </div>
-
                 </div>
-
-
             </div>
-
-
-        </section>
+        </section> -->
         <section id="section-729132" class="
                 widget-section widget-form-10 sec-padding-lg theme-dark-color-22                 " style="">
             <div class="container boxWidget">
@@ -189,7 +148,7 @@
                         <div class="richTextWidget page-heading ">
                             <h2>Get In Touch</h2>
                             <p>Reach out to Aishayy today to discuss your mobile app development needs. We are located
-                                in the vibrant city of New York and eager to collaborate on your next big project.</p>
+                                in the vibrant city of Lahore and eager to collaborate on your next big project.</p>
                         </div>
                         <div class="richTextWidget  ">
                             <p style="text-align:center;    font-weight: 500;"><span
@@ -202,27 +161,17 @@
                                     style="margin: 0px 15px;" class="fa">&#61505;</span><a href="#">Lahore, Punjab,
                                     Pakistan</a></p>
                         </div>
-
                     </div>
-
-
                 </div>
 
                 <div class="row centered-row ">
                     <div class="ContactFormWidget col-lg-8 centered-col ">
-
-
-
-
-
-
                         <form method="post" action="">
                             <div class="alert alert-success" style="display: none;" id="email_sent">
                                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                 <strong>Success!</strong> Your email has been sent successfully.
                             </div>
                             <div class="input">
-
                                 <label id="labelforname" class="">Your name:</label>
                                 <input type="text" name="name" required placeholder="&#61447; Name">
                             </div>
@@ -236,25 +185,14 @@
                             <button id="textforsubmitbtn" type="button" class="button theme-button"
                                 value="Submit">Submit
                             </button>
-
-
                         </form>
-
-
-
                     </div>
-
-
-
-
                 </div>
-
-
             </div>
-
-
         </section>
     </div>
+
+    <!-- footer -->
     <footer class="footerBefore wb-footer  wb-footer"
         style="background: url(//static.sitemantic.com/webbuilder/templates/bontan-zoo/images/bontan-zoo-footer.jpg) fixed no-repeat center center;position: relative; background-size: cover;">
         <div class="container boxWidget">
@@ -283,24 +221,13 @@
                         <p>Copyright &copy; 2024 All Rights Reserved | This template is made with by<strong
                                 style="color: #7575ff;"> Innove.ai</strong></p>
                     </div>
-
                 </div>
-
-
             </div>
-
-
         </div>
-
-
     </footer>
 
-
-
-
-
+    <!-- scripts -->
     <script src="https://static.sitemantic.com/webbuilder/js/lightbox/src/js/lightbox.js"></script>
-
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
@@ -343,8 +270,6 @@
             $(this).attr('href', $(this).attr('href') + "?innove");
         });
     </script>
-
-
     <script>
         var AppConfig = {
 
